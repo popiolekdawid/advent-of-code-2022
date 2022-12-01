892baa97f4b0b80ae88824a5a16cf3f38a22ec2f
Dawid Popiolek <dawidpopiolek@protonmail.com>
2022-12-01 15:07:55 +0100

    Completed Day1 Task2

--- day1/task2.js
+++ day1/task2.js
@@ -10,14 +10,13 @@ async function processData(filename) {
     for(i in arr) { //iterate over elves
         result.push(arr[i].split("\n")); //split into individual meals carried by each Elf
     }
-    console.log(result);
     return result;
   } catch (err) {
     console.log(err);
   }
 }
 
-function findElf (calorieData) { //find elf with the most calories
+function findThreeTopElvesCalories(calorieData) { //find elf with the most calories
   const sum = [];
   for (i in calorieData) {
       sum.push(0);
@@ -26,8 +25,10 @@ function findElf (calorieData) { //find elf with the most calories
     }
   }
   sum.pop(); //get rid of NaN at the end
-  return Math.max(...sum); //find the Elf with the biggest calorie sum
+  sum.sort();
+
+  return sum[sum.length-3] + sum[sum.length-4] + sum[sum.length-5]; //find the Elf with the biggest calorie sum
 }
 
-const calorieData = processData('./day1/task1/data.txt');  //read and process calorie data into a two dimensional array. (calorieData[index of Elf][snacks he carries]
-calorieData.then((result) => console.log(findElf(result))); //load the processed data, add each elfs snack's calorie values, find the biggest calorie sum and return its index.
+const calorieData = processData('./day1/data.txt');  //read and process calorie data into a two dimensional array. (calorieData[index of Elf][snacks he carries]
+calorieData.then((result) => console.log(findThreeTopElvesCalories(result))); //load the processed data, add each elfs snack's calorie values, find the biggest calorie sum and return its index.
