c3df0604dd8b42c0ef02ef40a0178d75dd986a56
Dawid Popiolek <dawidpopiolek@protonmail.com>
2022-12-02 11:45:32 +0100

    Completed Day2 Task1

---
+++ day2/task1.js
@@ -0,0 +1,55 @@
+const {readFileSync, promises: fsPromises} = require('fs');
+
+async function processData(filename) {
+  try {
+    const contents = await fsPromises.readFile(filename, 'utf-8');
+
+    const arr = contents.split(/\n/); //split data into individual rounds
+    const result = [];  //empty array to store the strategy
+    for(i in arr) { //iterate over rounds
+        result.push(arr[i].split(" ")); //split into columns
+    }
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function findTotalScore(strategyData) { //find elf with the most calories
+  let sum = 0;   //initialize sum with 0;
+  for(i in strategyData) {
+        switch(strategyData[i][1]) {
+            case 'X': sum += 1; //rock
+                if(strategyData[i][0] === 'A') { //rock -> draw
+                   sum += 3;
+                } else if(strategyData[i][0] === 'B') {  //paper -> loss
+                   sum += 0;
+                } else if(strategyData[i][0] === 'C') { //scissors -> win
+                   sum += 6;
+                }
+                break;
+            case 'Y': sum += 2; //paper
+                if(strategyData[i][0] === 'A') { //rock -> win
+                    sum += 6;
+                } else if(strategyData[i][0] === 'B') {  //paper -> draw
+                    sum += 3;
+                } else if(strategyData[i][0] === 'C') { //scissors -> loss
+                    sum += 0;
+                }
+                break;
+            case 'Z': sum += 3; //scissors
+                if(strategyData[i][0] === 'A') { //rock -> loss
+                    sum += 0;
+                } else if(strategyData[i][0] === 'B') {  //paper -> win
+                    sum += 6;
+                } else if(strategyData[i][0] === 'C') { //scissors -> draw
+                    sum += 3;
+                }
+                break;
+        }
+  }
+  return sum; //find the Elf with the biggest calorie sum
+}
+
+const strategyData = processData('./day2/data.txt');  //read and process calorie data into a two dimensional array. (strategyData[index of Elf][snacks he carries]
+strategyData.then((result) => console.log(findTotalScore(result))); //load the processed data, add each elfs snack's calorie values, find the biggest calorie sum and return its index.
