11860cc514788bef8447dcee8db1dbded13af1fb
Dawid Popiolek <dawidpopiolek@protonmail.com>
2022-12-03 23:35:15 +0100

    Day3 Task2 Completed

--- day3/task2.js
+++ day3/task2.js
@@ -1,46 +1,51 @@
-const {readFileSync, promises: fsPromises} = require('fs');
+let fs = require("fs");
+const input = fs.readFileSync("./day3/data.txt", "utf-8").toString();
 
-async function processData(filename) {
-  try {
-    const contents = await fsPromises.readFile(filename, 'utf-8');
+const backpackItemsList = input.split("\n");
 
-    const arr = contents.split(/\n/); //split data into rows
-    result = [];
-    for(i in arr) { 
-        result[i] = [];
-        result[i][0] = arr[i].substring(0, arr[i].length/2);
-        result[i][1] = arr[i].substring(arr[i].length/2, arr[i].length);
+// Helpers
+
+function findCommonCharacters(s1, s2, s3) {
+  for (let i in s1) {
+    let commonChar = s2.includes(s1[i]) && s3.includes(s1[i]) ? s1[i] : false;
+    if (commonChar != "") {
+      return commonChar;
     }
-    return result;
-  } catch (err) {
-    console.log(err);
   }
 }
 
-function findElement(data) {
-    result = [];
-    for(i in data) {
-        for(j in data[i][0]) {
-            if(data[i][0].includes(data[i][1][j])) {
-                result.push(data[i][1][j]);
-                break;
-            }
-        }
-    }
-    let sum = 0;
-    result.forEach(element => {
-        console.log("el: ", element);
-        console.log("code: ", element.charCodeAt(0));
-        let priority = element.charCodeAt(0);
-        if(priority >= 65 && priority <= 90){   //A to Z
-            sum += priority - 38;
-        } else if(priority >= 97 && priority <= 122) {  //a to z
-            sum += priority - 96;
-        }
-        console.log("sum: ", sum);
-    });
-    return sum;
-  }
+let countPoints = letter => {
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabetCapital = alpha.map(x => String.fromCharCode(x));
 
-const data = processData('./day3/data.txt');  //read and process data into a two dimensional array. strategyData[A,B,C][X,Y,Z]
-data.then((result) => console.log(findElement(result))); //show the result
+  const alphabetLowercase = alphabetCapital.map(a => a.toLocaleLowerCase());
+  if (alphabetCapital.includes(letter)) {
+    const points = alphabetCapital.indexOf(letter, 0) + 27;
+    return points;
+  }
+  if (alphabetLowercase.includes(letter)) {
+    const points = alphabetLowercase.indexOf(letter, 0) + 1;
+    return points;
+  }
+};
+
+// Solution for part 2
+
+const findSumOfAuthenticationBadges = input => {
+  counter = 0;
+  points = 0;
+  let group = [];
+  for (const backpack of input) {
+    group.push(backpack);
+
+    if (group.length == 3) {
+      let common = findCommonCharacters(group[0], group[1], group[2]);
+      let pointsToAdd = countPoints(common);
+      points += pointsToAdd;
+      group = [];
+    }
+  }
+  console.log(points);
+};
+
+findSumOfAuthenticationBadges(backpackItemsList);
