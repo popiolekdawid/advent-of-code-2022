8e0ec0fee7fd4b620adc82af33ef80f903eba388
Dawid Popiolek <dawidpopiolek@protonmail.com>
2022-12-08 17:52:59 +0100

    Completed day6 task1

--- day6/task1.js
+++ day6/task1.js
@@ -1,15 +1,15 @@
 const fs = require('fs');
 const data = fs.readFileSync('./day6/data.txt','utf8');
 
-let pakiet = [ data[0], data[1], data[2], data[3]];
-let count = 4;
+let packet = [ data[0], data[1], data[2], data[3]]; //initializing the packet, that will travel through the signal checking for uniqueness
+let count = 4; //initialize with 4 because we need to take into account the length of the packet init. above^
 
 for (let i = 4; i<data.length; i++) {
-  if (!(pakiet.includes(data[i])) && (pakiet.length === new Set(pakiet).size)) {
+  if (!(packet.includes(data[i])) && (packet.length === new Set(packet).size)) {  //check if the new character is present in current packet and check if the characters in packet are different to eachoter.
     break;
   } else {
-    pakiet.shift();
-    pakiet.push(data[i]);
+    packet.shift();   //throw out the first char of the packet
+    packet.push(data[i]);   //add the current char to the end of the packet
     count++;
   }
 }
